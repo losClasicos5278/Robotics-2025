1efe3a5c913dbd20879008f85d3eb2db59da1c2c
losClasicos5278 <152567506+losClasicos5278@users.noreply.github.com>
2025-02-24 18:14:04 -0600

    first draft. fix encoder

--- Robot-2025/src/main/java/frc/robot/Robot.java
+++ Robot-2025/src/main/java/frc/robot/Robot.java
@@ -142,6 +142,7 @@
 
 package frc.robot;
 
+import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -231,7 +232,7 @@ public class Robot extends TimedRobot {
 
 
     // Get encoder velocity (in ticks per second)
-    encoderVelocity = encoder.getRate();
+    encoderPosition = encoder.get();
     // Log the encoder position and velocity
     System.out.println("Encoder Position: " + encoderPosition);
   }
