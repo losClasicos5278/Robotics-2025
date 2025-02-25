e4d8015bd11b61e351f00c45b7afc639f26048b6
losClasicos5278 <152567506+losClasicos5278@users.noreply.github.com>
2025-02-24 18:32:43 -0600

    fix 2

--- Robot-2025/src/main/java/frc/robot/Robot.java
+++ Robot-2025/src/main/java/frc/robot/Robot.java
@@ -143,7 +143,6 @@
 package frc.robot;
 
 import edu.wpi.first.wpilibj.Encoder;
-import edu.wpi.first.wpilibj.Encoder;
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
